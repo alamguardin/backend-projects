6c278899df0aa9c3ef23eb438ca6916ba49751e4
Alam Guardin <alamguardin@gmail.com>
2025-10-19 12:59:36 -0500

    feat: add CORS Middleware

--- 00-users-crud-api/backend/src/index.ts
+++ 00-users-crud-api/backend/src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 import usersRoute from './routes/usersRoute';
 
 const app = new Hono();
+
+app.use('*', cors());
 
 app.get('/', (c) => {
 	return c.json({
--- 00-users-crud-api/frontend/src/main.js
+++ 00-users-crud-api/frontend/src/main.js
@@ -1,1 +1,16 @@
 import './style.css';
+
+async function getData() {
+	const url = 'http://localhost:3000';
+	try {
+		const response = await fetch(url);
+		if (!response.ok) throw new Error(response.status);
+
+		const result = await response.json();
+		console.log(result);
+	} catch (error) {
+		console.log(error.message);
+	}
+}
+
+getData();
