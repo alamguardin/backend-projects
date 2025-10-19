4f205ad5260d744747a1efc52ec1939cfebe8db7
Alam Guardin <alamguardin@gmail.com>
2025-10-19 13:38:04 -0500

    feat: get and display users from the API

--- 00-users-crud-api/frontend/src/main.js
+++ 00-users-crud-api/frontend/src/main.js
@@ -7,10 +7,25 @@ async function getUsersList() {
 		if (!response.ok) throw new Error(response.status);
 
 		const result = await response.json();
-		console.log(result);
+		return result;
 	} catch (error) {
 		console.log(error.message);
 	}
 }
 
-getUsersList();
+const usersList = await getUsersList();
+const usersListElement = document.getElementById('usersList');
+const userCardTemplate = document.getElementById('userTemplateCard');
+const usersListFragment = new DocumentFragment();
+
+for (const user of usersList) {
+	const clone = userCardTemplate.content.cloneNode(true);
+	const span = clone.querySelectorAll('span');
+	span[0].textContent = user.id;
+	span[1].textContent = user.name;
+	span[2].textContent = user.email;
+
+	usersListFragment.appendChild(clone);
+}
+
+usersListElement.appendChild(usersListFragment);
